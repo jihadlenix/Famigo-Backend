5cd0c14eba4a4bd39485d1ce53a6f0f15c31eb89
jihadlenix <jihadismasarani@gmail.com>
2025-10-19 17:53:26 +0300

    finished authentication

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="FAMIGO_", env_file=".env", extra="ignore")
-    DATABASE_URL: str = "sqlite:///C:/Users/AUB/Famigo-Backend/famigo.db"
+    DATABASE_URL: str = "sqlite:///C:/Users/maasa/Desktop/Famigo-Backend/famigo.db"
 
     SECRET_KEY: str = "change-me"
     ACCESS_TOKEN_MIN: int = 60 * 24
